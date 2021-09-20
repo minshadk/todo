61ffd1880f08d71013b2655a8924815cdbcd5260
Signmeingit <signmein365@gmail.com>
2021-09-20 23:40:24 +0530

    changes added

--- app.js
+++ app.js
@@ -1,51 +1,61 @@
-document.querySelector("form").addEventListener("submit", handleSubmitForm);
+// Selectors
 
+// Event Listners
+document
+  .querySelector(".form-submit")
+  .addEventListener("submit", handleSubmitForm);
+// getTodos fuction will exuecute when page load
+// Event lister for page loaging DOMContentLoaded
 document.addEventListener("DOMContentLoaded", getTodos);
 
 let type = document.getElementById("type").textContent;
-let form = document.querySelector(".form");
-// console.log(form.classList);
-document.querySelector(".add-btn").addEventListener("click", function () {
-  console.log("Add button is clicked");
-});
 
+// handleSubmitForm will execute when a form is submited
 function handleSubmitForm(event) {
+  //to avoid page referes when from is submited
+  // page referesing when form  is submited is browser default
   event.preventDefault();
 
   // copying value of title and description to variable title and description
   let title = document.getElementById("title").value;
   let description = document.getElementById("description").value;
+  let form = document.querySelector(".form-submit");
   let todo;
 
+  // checking if titel feild or description feild is empty
   if (title != "" && description != "") {
-    // Creating and storeing value of title and description to Object todo
+    // Creating a object todo and storeing value of title and description to  todo
     const todo = {
       title: title,
       description: description,
       type: type,
-      // Genrating random id
-      id: (id = (Date.now() + "").slice(-10)),
     };
     // Saveing  todo object to localSotrage
     saveToLocal(todo);
     console.log(todo);
   }
-  document.getElementById("form").reset();
+  // Reseting form feildes
+  form.reset();
 }
 
 // code for saveing to localStorage
 function saveToLocal(todo) {
   //CHECKING ALREADY HAVE A VALUE
   let todos;
+  // if localstorage  is empty a new array todos is created
   if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
+  // values already in the localstorage is copied to todos
     todos = JSON.parse(localStorage.getItem("todos"));
   }
+  // adding the latest todo to the array todos
   todos.push(todo);
+  // Saveing latest todos array to localstorage
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
+// Fuction for listing todo cards
 function getTodos() {
   let todos;
   if (localStorage.getItem("todos") === null) {
@@ -55,12 +65,13 @@ function getTodos() {
   }
 
   todos.forEach(function (todo) {
-    // flitering card according to todo type
+    // flitering cards according to todo type
     if (todo.type === type) {
-      // Code for genrating a list item
+
       let ul = document.querySelector("ul");
       let li = document.createElement("li");
 
+      // Code for genrating a card item
       li.innerHTML = `
       <span class="title">${todo.title}</span>
       <div class="description"> ${todo.description}</div>
